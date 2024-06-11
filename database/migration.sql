dc1027655d5ab4a3bd341c496253e5f478d87622
Yevhen Hrytsai <yevgen.grytsay@gmail.com>
2024-06-11 15:13:03 +0300

    update migration

--- database/migration.sql
+++ database/migration.sql
@@ -1,5 +1,23 @@
+USE app;
 
 create table tree_1 (
     id int unsigned not null auto_increment primary key,
     json text
 );
+
+INSERT INTO tree_1 (id, json) VALUES (1, '{
+  "id": 1,
+  "name": "Node #1",
+  "children": [
+    {
+      "id": 2,
+      "name": "Node #2",
+      "children": []
+    },
+    {
+      "id": 3,
+      "name": "Node #3",
+      "children": []
+    }
+  ]
+}');
