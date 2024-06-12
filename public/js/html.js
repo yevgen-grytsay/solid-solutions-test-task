5b65aa99e29f419a338c48fedea50a1ea0986281
Yevhen Hrytsai <yevgen.grytsay@gmail.com>
2024-06-12 16:21:01 +0300

    move renderer to separate class

--- public/js/html.js
+++ public/js/html.js
@@ -1,8 +1,58 @@
 /* global $ */
 
+class Renderer {
+    counter = 0
+    storage = {}
+
+    nextId = () => {
+        ++this.counter
+
+        return this.counter
+    }
+
+    render(templateFnc) {
+        const renderer = this
+
+        const renderedTemplate = templateFnc({
+            assign: (key, data) => {
+                const id = renderer.nextId()
+                this.storage[id] = data
+                return `data-element-id="${id}" data-element-key=${key}`
+            },
+        })
+
+        const $el = $('<div></div>').append($(renderedTemplate))
+
+        return $el.html()
+    }
+
+    assign($el) {
+        $el.find('[data-element-id]').each((index, el) => {
+            const id = $(el).data('elementId')
+            const key = $(el).data('elementKey')
+
+            $(el).removeAttr('data-element-id').removeAttr('data-element-key')
+
+            $(el).data({
+                [key]: this.storage[id],
+            })
+        })
+    }
+}
+
+let renderer = null
+
 export default {
     e(string) {
         return $('<div>').text(string).html()
+    },
+
+    rendererSingleton() {
+        if (!renderer) {
+            renderer = new Renderer()
+        }
+
+        return renderer
     },
 
     data(data) {
--- public/js/tree-component.js
+++ public/js/tree-component.js
@@ -9,10 +9,14 @@ class TreeComponent {
     $container
     tree
     $treeContainer
+    r
+    renderer
 
     constructor($container) {
         this.tree = null
         this.$container = $container
+        this.renderer = html.rendererSingleton()
+        this.r = this.renderer.render.bind(this.renderer)
     }
 
     init() {
@@ -23,15 +27,13 @@ class TreeComponent {
         const $bus = $(this)
 
         this.$container.on('click', '.item-add', e => {
-            const $el = $(e.target)
-            const id = $el.closest('.item').data('id')
+            const id = $(e.target).data('node_id')
 
             $bus.trigger(TreeComponent.EVENT_ADD_NODE, [{ id }])
         })
 
         this.$container.on('click', '.item-delete', e => {
-            const $el = $(e.target)
-            const id = $el.closest('.item').data('id')
+            const id = $(e.target).data('node_id')
 
             $bus.trigger(TreeComponent.EVENT_DELETE_NODE, [{ id }])
         })
@@ -45,9 +47,9 @@ class TreeComponent {
 
         this.$container.on('click', '.item-save', e => {
             const $el = $(e.target)
-            const item = $el.closest('.item')
-            const id = $el.closest('.item').data('id')
-            const newName = item.find('p.text .name').text()
+            const $item = $el.closest('.item')
+            const id = $el.data('node_id')
+            const newName = $item.find('p.text .name').text()
 
             $el.closest('.item')
                 .find('p.text .name')
@@ -61,9 +63,11 @@ class TreeComponent {
 
     render() {
         const $treeContainer = $('<div class="tree-container"></div>')
-        this.#createNodes([this.tree.root], $treeContainer)
+        const $el = this.#createNodes([this.tree.root])
+        $treeContainer.append($el)
         this.$treeContainer.replaceWith($treeContainer)
         this.$treeContainer = $treeContainer
+        this.renderer.assign($treeContainer)
     }
 
     setState(tree) {
@@ -71,32 +75,37 @@ class TreeComponent {
     }
 
     #renderNode(child) {
-        return `
-            <div class="item" ${html.data({ id: child.id, child })}>
-                <p class="text">
-                    <span class="name">${html.e(child.name)}</span>
-                    <span ${html.attrs({ class: 'controls' })}>
-                       <button type="button" class="btn btn-light item-add">+</button>
-                       <button type="button" class="btn btn-light item-delete">-</button>
-                       <button type="button" class="btn btn-light item-edit">edit</button>
-                       <button type="button" class="btn btn-light item-save">save</button>
-                    </span>
-                </p>
-                <div class="children">
-                    ${html.map(child.children, child => {
-                        return this.#renderNode(child)
-                    })}
+        return this.r(({ assign }) => {
+            return `
+                <div 
+                    class="item"
+                    ${assign('node', child)}
+                >
+                    <p class="text">
+                        <span class="name">${html.e(child.name)}</span>
+                        <span ${html.attrs({ class: 'controls' })}>
+                           <button type="button" class="btn btn-light item-add" ${assign('node_id', child.id)}>+</button>
+                           <button type="button" class="btn btn-light item-delete" ${assign('node_id', child.id)}>-</button>
+                           <button type="button" class="btn btn-light item-edit" ${assign('node_id', child.id)}>edit</button>
+                           <button type="button" class="btn btn-light item-save" ${assign('node_id', child.id)}>save</button>
+                        </span>
+                    </p>
+                    <div class="children">
+                        ${html.map(child.children, child => {
+                            return this.#renderNode(child)
+                        })}
+                    </div>
                 </div>
-            </div>
-        `
+            `
+        })
     }
 
-    #createNodes(nodesStateList, $treeContainer) {
+    #createNodes(nodesStateList) {
         const parts = nodesStateList.map(child => {
             return this.#renderNode(child)
         })
-        const $el = $(parts.join('\n'))
-        $treeContainer.append($el)
+
+        return $(parts.join('\n'))
     }
 }
 
